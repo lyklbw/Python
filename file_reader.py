e27a086a7b949b995b44762360c283d59d7d0e40
lyklbw <1512972802@qq.com>
2023-08-08 11:27:30 +0800

    8.8

---
+++ file_reader.py
@@ -0,0 +1,13 @@
+#写一个open就可以了，python会自己判断close的时机
+#文件读取
+with open('pi_digits.txt') as file_object:
+	contents = file_object.read()
+	print(contents)
+#逐行读取
+filename='pi_digits.txt'
+with open(filename) as file_object:
+    for line in file_object:
+        print(line)#print(line.rstrip()) 消除多余的空白行
+#创建一个包含文件各行内容的列表
+with open(filename) as file_object
+	lines=file_object.readlines()#从文件中读取一行,在with代码块之外 依旧就可以使用
