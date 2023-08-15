edbcfd82ce54745f310c2b5b0278541fd5cb034b
lyklbw <1512972802@qq.com>
2023-08-15 11:36:47 +0800

    pyst

--- division.py
+++ division.py
@@ -1,4 +1,20 @@
-try:
-	print(5/0)
-except ZeroDivisionError:
-	print("you can not divide by 0")
+#try:
+#	print(5/0)
+#	except ZeroDivisionError:
+#	print("you can not divide by 0")
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+while True:
+	first=input("\nFirst number: ")
+	if first=='q':
+		break
+	second=input("Second number: ")
+	if second=='q':
+		break
+	try:
+		answer=int(first)/int(second)	
+	except ZeroDivisionError:
+		print("you can not divide by 0")
+	else:
+		print(answer)
+
