2f22b829f729e5641499cc41b73f5e930aa8def3
lyklbw <1512972802@qq.com>
2023-08-16 16:56:40 +0800

    test class

--- test_survey.py
+++ test_survey.py
@@ -2,10 +2,27 @@ import unittest
 from survey import AnonymousSurvey
 
 class TestAnonymousSurvey(unittest.TestCase):
+    #setUp()方法，创建一个调查对象和一组答案，供使用的测试方法使用
+
+    """def setUp(self):
+           创建一个调查对象和一组答案，供使用的测试方法使用
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)#创建一个调查对象 
+        self.responses = ["English","Spanish","Mandarin"]#创建一组答案
+    """
     def test_store_single_response(self):
         """测试单个答案能否会被妥善地存储"""
         question = "What language did you first learn to speak?"
         my_survey = AnonymousSurvey(question)
         my_survey.store_response("English")
         self.assertIn("English",my_survey.responses)#测试是否在列表中
-unittest.main()
+    def test_store_three_responses(self):
+        """测试三个答案能否会被妥善地存储"""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ["English","Spanish","Mandarin"]
+        for response in responses:
+            my_survey.store_response(response)
+        for response in responses:
+            self.assertIn(response,my_survey.responses)
+unittest.main()#此时完成了两次测试，每次测试都会运行test_store_single_response()和test_store_three_responses()，并报告每个测试都通过了。
