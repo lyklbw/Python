b2479d6d1676b3416a677095d406fe857526bb91
lyklbw <1512972802@qq.com>
2023-08-20 08:41:24 +0800

    gamr program

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,17 +1,43 @@
-import sys #use it to exit the game when the player quits
+
+
 import pygame
+from pygame.sprite import Group
+from setting import Setting
+from ship import Ship
+from alien import Alien
+import game_functions as gf     
+
 def run_game():
+    
     # Initialize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion")
-    bg_color = (230, 230, 230)
+    #引入Setting类便于管理参数
+    ai_setting = Setting()
+    # Set the background color white.
+    bg_color = (ai_setting.bg_color)
+    #创建一个名为screen的显示窗口，实参（1200，800）是一个元组，指定游戏窗口尺寸
+    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
+    
+    #引入Ship类,关于ship操作都在其中
+    ship=Ship(ai_setting,screen)
+    #use Group to store bullets and lyy
+    bullets = Group()
+    aliens=Group()
+    #对游戏界面进行简单设置
+    pygame.display.set_caption("Ikun Invasion")
+    pygame.display.set_icon(pygame.image.load('images/ship.bmp'))
+
+    gf.creat_fleet(ai_setting,screen,ship,aliens)
+    
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(bg_color)
-        pygame.display.flip()
-run_game()
+        gf.check_events(ai_setting,screen,ship,bullets)
+        #通过标志判断是否移动并执行
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_setting,aliens)
+        # Redraw the screen during each pass through the loop.        
+        gf.update_screen(ai_setting, screen, ship,aliens,bullets)
+
+run_game()  
